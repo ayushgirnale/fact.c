de1a545c787c260be8cc09e0613b8fc18c9c6ab3
ayushgirnale <121081940+ayushgirnale@users.noreply.github.com>
2022-12-20 12:51:42 +0530

    first commit

--- fact.c
+++ fact.c
@@ -1,2 +1,15 @@
 #include<stdio.h>
+void fact();
+main()
+{
+  fact():
+}
 void fact()
+{
+  int n,i,f=1;
+  printf("\n enter n=");
+  for(i=1;i<=n;i++)
+  {
+    f=f*i;
+  }
+  printf("\n factorial=%d
